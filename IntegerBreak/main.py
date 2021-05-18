488d02471b13ef288e4b768922435f20e830d034
Alfred M <alfredmjz@gmail.com>
2021-05-18 18:50:35 +0800

    Added comments and docstring

--- IntegerBreak/main.py
+++ IntegerBreak/main.py
@@ -1,17 +1,31 @@
-def integerBreak(self):
-    if(self == 1 or self == 2):
+def integerBreak(x):
+    """
+    Returns the maximum product from the total added value of a positive integer.
+    Example: 2+2+3 = 7 -> 2*2*3 = 12
+
+    >>> integerBreak(7)
+
+    12
+    """
+
+    if(x == 1 or x == 2):
         return 1
-    elif (self == 3):
+    elif (x == 3):
         return 2
     else:
-        remainder = self % 3
-        exponent = self // 3
+        remainder = x % 3
+        exponent = x // 3
+
+        # Remainder can only be 0, 1 or 2
         if(remainder == 1):
             exponent -= 1
             res = 3 ** exponent
+            # Subtraction of res and x is always 4
             res *= 4
         else:
             res = 3 ** exponent
-            if(remainder != 0):
+            # If remainder is 0, then the exponent is the total added value
+            # We only need to multiply the remaining value when remainder exist
+            if(remainder == 2):
                 res *= remainder
     return res
